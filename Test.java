d1b1327dec69711d641273846570a2f21c3b96b5
profkkshukla <profkapilshukla@gmail.com>
2022-05-02 16:20:29 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test
 {
     public static void main(String args[])
     {
+	System.out.println("Welcome to My Program");
         for(String s:args)
         {
             System.out.println(s);
