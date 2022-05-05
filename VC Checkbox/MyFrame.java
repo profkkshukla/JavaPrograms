16efa35df8dee2d489c2f1f939cdc84b7205ae5f
profkkshukla <profkapilshukla@gmail.com>
2022-05-05 14:06:50 +0530

    Create MyFrame.java

---
+++ VC Checkbox/MyFrame.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import java.lang.*;
+
+public class MyFrame extends Frame
+{
+	Checkbox c1, c2, c3;
+	Font f1;
+	MyActionListener ml = new MyActionListener(this);
+	MyFrame()
+	{
+		super("My Test Application");
+		setLayout(null);
+		setBounds(0,0,1260,1000);
+		
+		c1 = new Checkbox("ICP");
+		c2 = new Checkbox("OOP");
+		c3 = new Checkbox("IWT");
+		
+		add(c1);
+		add(c2);
+		add(c3);
+		
+		c1.setBounds(100,100,400,100);
+		c2.setBounds(100,300,400,100);
+		c3.setBounds(100,500,400,100);
+		
+		f1 = new Font("Times",Font.BOLD,50);
+		
+		c1.setFont(f1);
+		c2.setFont(f1);
+		c3.setFont(f1);
+		
+		c1.addItemListener(ml);
+		c2.addItemListener(ml);
+		c3.addItemListener(ml);
+		
+		addWindowListener(new WindowAdapter()
+			{
+				public void windowClosing(WindowEvent we)
+				{
+					System.exit(0);
+				}
+			});
+	}
+	public static void main(String args[])
+	{
+			MyFrame mf = new MyFrame();
+			mf.setVisible(true);
+	}
+}
