1d4e1cc468e858f6449134b0287031247e1410c4
profkkshukla <profkapilshukla@gmail.com>
2022-05-05 14:48:23 +0530

    Update MyActionListener.java

--- VC Checkbox/MyActionListener.java
+++ VC Checkbox/MyActionListener.java
@@ -3,24 +3,37 @@ import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
 import java.lang.*;
+import java.util.*;
 
-public class MyActionListener implements ItemListener
+public class MyActionListener implements ItemListener, ActionListener
 {
 	MyFrame mf;
+	ArrayList <String> al;
 	
 	MyActionListener(MyFrame m){
 		this.mf = m;
+		al = new ArrayList<String>();
 	}
 	
 	public void itemStateChanged(ItemEvent e) 
 	{
 		if(e.getStateChange()==e.SELECTED)
 		{
-			System.out.println(e.getItem().toString() +" is Selected");
+			al.add(e.getItem().toString());
+			//System.out.println(e.getItem().toString() +" is Selected");
 		}
 		else
 		{
-			System.out.println(e.getItem().toString() +" is Deselected");
+			al.remove(e.getItem().toString());
+			//System.out.println(e.getItem().toString() +" is Deselected");
+		}
+	}
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		for(String s:al)
+		{
+			System.out.println(s);
 		}
 	}
 	
